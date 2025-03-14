765d3a2e362226603ebf559726b0e429db6ce44e
sebastian Quiroga <sebasquirogasb@gmail.com>
2025-03-13 22:42:13 -0500

    Se organizo el dashboard
    Se organizo y crearon las opciones del dashboard, se dio forma mas estetica al boton de logout y se creo el espacio para operar con los fetch

--- JS/validations.js
+++ JS/validations.js
@@ -13,3 +13,27 @@ async function test() {
         credentials: 'include'
     }).then(response => {alert("Test de cookie")})
 }
+
+function showContent(option) {
+    const contentSection = document.getElementById("content-area");
+    let content = "";
+    
+    switch(option) {
+        case "Productos":
+            content = "<h2>Productos</h2><p>Lista de productos.</p>";
+            break;
+        case "Proveedores":
+            content = "<h2>Proveedores</h2><p>Opciones de proveedores</p>";
+            break;
+        case "Tienda":
+            content = "<h2>Tienda</h2><p>Informacion de la tienda.</p>";
+            break;
+        case "Lista":
+            content = "<h2>Lista</h2><p>Lista de hoy</p>";
+            break;   
+        default:
+            content = "<h2>Bienvenido</h2><p>Selecciona una opción del menú.</p>";
+    }
+    
+    contentSection.innerHTML = content;
+}
