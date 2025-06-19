3ffb751e192a19074aa288abf7a6543fde95a875
sebastian Quiroga <sebasquirogasb@gmail.com>
2025-06-19 12:42:29 -0500

    Se modifica la pestana de listado

--- JS/components/lista.js
+++ JS/components/lista.js
@@ -1,6 +1,12 @@
+function mostrarFormulario (string) {const form = document.getElementById(string) /*ponerle un try catch para que cuando no exita el formulario siga en pie*/
+    form.style.display = form.style.display === "none" ? "block" : "none";
+  }
+
 export function init() {
-  const addButton = document.getElementById('add-list-btn');
+  const addButton = document.getElementById('add-lista-btn');
   addButton?.addEventListener('click', () => {
-    alert('Agregar nueva lista');
+    mostrarFormulario('createLista');
   });
+
+
 }
--- JS/components/proveedores.js
+++ JS/components/proveedores.js
@@ -4,8 +4,42 @@ function mostrarFormulario (string) {const form = document.getElementById(string
 
 
 export function init() {
-  const addButton = document.getElementById('add-provider-btn');
-  addButton?.addEventListener('click', () => {
-    alert('Agregar nuevo producto');
-  });
-}
+
+
+  const findButton = document.getElementById('search-provider-btn');
+    findButton?.addEventListener('click', () => {
+    mostrarFormulario('findProvider');
+    });
+  
+  const createButton = document.getElementById('create-provider-btn');
+    createButton?.addEventListener('click', () => {
+    mostrarFormulario('createProvider');
+ });
+/*funciones de los botones de proveedores*/
+
+document.getElementById('findProvider')?.addEventListener('submit', async function(event) {
+    event.preventDefault();
+  const providerName = document.getElementById("providerName").value;
+  console.log(providerName);
+  await fetch(`http://localhost:3000/api/providers/provider`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: JSON.stringify({ provider: providerName })
+  }).then(response => {
+    if (response.status === 201) {response.json()
+    } else if (response.status === 404) {
+      alert("Proveedor no encontrado");
+    } else {
+      alert("Error al buscar el proveedor");
+    }
+  }).catch(error => {
+    alert("Error al buscar el proveedor: " + error.message);
+  })
+})
+
+
+} /* corchete del init */
